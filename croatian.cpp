385c09dc9830d7272547420556f45f54829f5f8c
Dalekamistoso (DrWh0) <67012746+Dalekamistoso@users.noreply.github.com>
2021-01-16 11:46:46 +0100

    Delete croatian.cpp

--- croatian.cpp
+++
@@ -1,6 +0,0 @@
-char* AboutInfo="Nintendo ne podržava ovaj program niti ga podržava, a autor nije povezan ni s jednim drugim korporativnim entitetom.\r\n"
-"Program je freeware i pod uvjetom \"AS IS\"...\r\n"
-"Autor ne može biti odgovoran za štetu bilo koje vrste koja proizlazi iz njegove uporabe ili prisutnosti.\r\n";
-
-char* CorruptText="Integritet dosjea ovog programa je kompromitiran.  Ili su mediji na koje je program pohranjen ošteceni ili je nešto izmijenilo datoteku.  Pokušajte preuzeti svježu kopiju programa.  Možda bi bilo mudro skenirati virus.\r\n\r\nPokrenuti program?";
-char* CorruptTitle="Datoteka je korumpirana!";
